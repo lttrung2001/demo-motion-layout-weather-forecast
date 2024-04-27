d7c7b6ad73ad66d23cfbeb0c63e83c3ec6328ef5
VPMN/trunglt <trunglt@vnpay.vn>
2024-04-27 21:03:03 +0700

    Initial commit

---
+++ app/src/main/java/vn/trunglt/demomotionlayoutweatherforecast/MainActivity.kt
@@ -0,0 +1,22 @@
+package vn.trunglt.demomotionlayoutweatherforecast
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        findViewById<MotionLayout>(R.id.main).transitionToEnd()
+    }
+}
